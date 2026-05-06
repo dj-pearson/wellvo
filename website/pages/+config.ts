eda8f73b24820e4c2e2470fa341c7a8e28e16435
dj-pearson <pearson.performance@gmail.com>
2026-05-06 15:27:48 -0500

    feat(config): update build script to include prerendering and adjust prerender settings

--- website/pages/+config.ts
+++ website/pages/+config.ts
@@ -1,7 +1,7 @@
 import type { Config } from 'vike/types'
 
 export default {
-  prerender: true,
+  prerender: { disableAutoRun: true },
   clientRouting: true,
   hydrationCanBeAborted: true,
   passToClient: ['routeParams', 'urlPathname'],
